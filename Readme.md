6e6366c5cff2833e7c6f8ee0661a567acb5ab7c0
maki <ginger.miao.sesame@gmail.com>
2021-12-17 15:34:47 +0800

    init

---
+++ Readme.md
@@ -0,0 +1,5 @@
+
+
+Date: 2020-12-17
+Project: 01
+Level: 1, hello world project
---
+++ scripts/main.js
@@ -0,0 +1,48 @@
+
+
+// title, main-pic logic
+let titleToggle = true
+let titleEn = "LARK is amazing!"
+let titleCn = "飞书, 做的很不错!"
+
+let picToggle = true
+let mainPics = ["images/lark-logo.jpeg", "images/lark-logo-v2.jpeg"]
+let picId = "main-pic"
+let attributeName = "src"
+
+setInterval(function(){
+    let myHeading = document.querySelector('#main-title');
+    if (titleToggle) {
+        myHeading.textContent = titleEn;
+        document.getElementById(picId).setAttribute(attributeName, mainPics[1])
+        titleToggle = false;
+    }else{
+        myHeading.textContent = titleCn;
+        document.getElementById(picId).setAttribute(attributeName, mainPics[0])
+        titleToggle = true;
+    }
+}, 3000);
+
+// user information record
+function recordName() {
+    var name = document.getElementById("name").value
+    console.log("get name "+name)
+    if(!name || name === null) {
+        alert("please input correct name")
+    }else{
+        console.log("enters store name "+name)
+        window.localStorage.setItem('name', name);
+    }
+}
+document.getElementById("name-button").onclick = recordName
+
+function welcome() {
+        let name = window.localStorage.getItem('name');
+    if (!name || name === null) {
+        document.getElementById("welcome-old").innerHTML = "Welcome to our site !"
+    }else{
+        document.getElementById("welcome-old").innerHTML = "Hi <b>"+name+"</b>, nice to meet you !"
+    }
+}
+welcome()
+
